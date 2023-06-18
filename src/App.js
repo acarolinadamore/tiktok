715d3f2de302482b71159a4124ad8c6a68f8737b
acarolinadamore@gmail.com <acarolinadamore@gmail.com>
2023-06-18 11:04:36 -0300

    aula 04

--- src/App.js
+++ src/App.js
@@ -5,9 +5,33 @@ function App() {
   return (
     <div className="App">
       <div className="app-videos">
-        <Video/>
-        <Video/>
-        <Video/>
+        
+        <Video
+        likes={111}
+        messages={222}
+        shares={333}
+        name="Ana Carolina"
+        description="Gato goleiro"
+        music="musica épica"
+        url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z"
+        />
+        <Video
+        likes={444}
+        messages={555}
+        shares={777}
+        name="Ana Carolina"
+        description="Gato goleiro"
+        music="musica épica"
+        />
+        <Video
+        likes={888}
+        messages={1010}
+        shares={1111}
+        name="Ana Carolina"
+        description="Gato goleiro"
+        music="musica épica"
+        />
+        
       </div>
     </div>
   );
--- src/pages/components/footer/VideoFooter.js
+++ src/pages/components/footer/VideoFooter.js
@@ -2,17 +2,17 @@ import React from 'react'
 import "./videoFooter.css"
 import MusicNoteIcon from '@mui/icons-material/MusicNote'
 
-function VideoFooter() {
+function VideoFooter({name,description,music}) {
   return (
     <div className="videoFooter">
       <div className="videoFooter-text">
-        <h3>@Ana Damore</h3>
-        <p>Descrição do Vídeo</p>
+        <h3>@{ name }</h3>
+        <p>{ description }</p>
           <div className='videoFooter-music'>
               <MusicNoteIcon
               className="videoFooter-icon"/>
               <div className="videoFooterMusic-text">
-                <p>Titulo da musica</p>
+                <p>{music}</p>
               </div>
           </div>
         </div>
